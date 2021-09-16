2257c4b26bd9ea226160d4315b2932f137a252e9
Enrique Ten <kike10bru@gmail.com>
2021-09-16 11:29:00 +0200

    fix bug with adapters. Use holder instead of a bad abstraction of binding

--- shoppingcart/src/main/java/com/jeff/pizza/cart/presentation/ui/ShoppingCartProductsAdapter.kt
+++ shoppingcart/src/main/java/com/jeff/pizza/cart/presentation/ui/ShoppingCartProductsAdapter.kt
@@ -3,24 +3,26 @@ package com.jeff.pizza.cart.presentation.ui
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.jeff.pizza.cart.databinding.ShoppingCartProductBinding
 import com.jeff.pizza.cart.presentation.model.ShoppingCartProductUI
 import com.jeff.pizza.core.presentation.ui.BaseAdapter
-import com.jeff.pizza.cart.databinding.ShoppingCartProductBinding
 
 class ShoppingCartProductsAdapter(
         private val products: MutableList<ShoppingCartProductUI>
-): BaseAdapter<ShoppingCartProductsAdapter.ViewHolder, ShoppingCartProductBinding, ShoppingCartProductUI>(products) {
+): BaseAdapter<ShoppingCartProductsAdapter.ViewHolder, ShoppingCartProductUI>(products) {
 
-    inner class ViewHolder(binding: ShoppingCartProductBinding): RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(binding: ShoppingCartProductBinding): RecyclerView.ViewHolder(binding.root) {
+        val titleProductShoppingCart = binding.titleProductShoppingCart
+        val pricesProductShoppingCart = binding.pricesProductShoppingCart
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        setBinding(ShoppingCartProductBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-        return ViewHolder(binding)
+        return ViewHolder(ShoppingCartProductBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val product = products[position]
-        with(binding) {
+        with(holder) {
             titleProductShoppingCart.text = product.name
             pricesProductShoppingCart.adapter = ShoppingCartPricesAdapter(product.prices.toMutableList())
         }
